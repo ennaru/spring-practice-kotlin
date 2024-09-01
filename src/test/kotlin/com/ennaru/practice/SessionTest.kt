dcca6389cd0d01b6b7d49960c9448cf1a93944b2
ennaru <enharu@naver.com>
2024-09-01 18:31:12 +0900

    sessionTest 설명추가

--- src/test/kotlin/com/ennaru/practice/SessionTest.kt
+++ src/test/kotlin/com/ennaru/practice/SessionTest.kt
@@ -27,9 +27,12 @@ class SessionTest {
 
     }
 
+    // TestCode 에서 사용하는 Session Class는 MockHttpSession입니다.
     @Test
     @DisplayName("세션 메모리 테스트")
     fun sessionMemory() {
+
+        // 세션의 attribute도 결국 Map에 담기 때문에 해지 시 자동으로 메모리에서 해지되지 않습니다.
 
         // ExecutorService 생성
         val count = 20000
@@ -60,6 +63,7 @@ class SessionTest {
         log.info("USED_MEMORY: ${Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()}")
 
         // session remove
+        // removeAttribute 는 [map]의 remove 함수를 호출하지만
         for(i in 1..count) {
             executorService.submit {
                 try {
@@ -77,7 +81,7 @@ class SessionTest {
         log.info("FREE_MEMORY: ${Runtime.getRuntime().freeMemory()}")
         log.info("USED_MEMORY: ${Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()}")
 
-
+        // invalidate를 호출하면 clearAttributes() 를 호출하는데, attribute 들을 전부 해지시킴
         (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request.session?.invalidate()
 
         log.info("==================After Invalidate======================")
