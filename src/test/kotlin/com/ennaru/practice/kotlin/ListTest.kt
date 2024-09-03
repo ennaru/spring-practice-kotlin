3291a23ac3069d65a9d5426f6faa7b3b489a0fad
ennaru <enharu@naver.com>
2024-09-03 20:55:27 +0900

    lateinit 예시, iterator 함수 예시 추가

--- src/test/kotlin/com/ennaru/practice/kotlin/ListTest.kt
+++ src/test/kotlin/com/ennaru/practice/kotlin/ListTest.kt
@@ -15,19 +15,19 @@ class ListTest {
 
     @BeforeTest
     fun before() {
-        log.info("[Kotlin Test] ${javaClass.simpleName}")
+        log.info("[Kotlin Test] ${javaClass.simpleName} Start")
     }
 
     @AfterTest
     fun after() {
-        log.info("=====================================")
+        log.info("[Kotlin Test] ${javaClass.simpleName} End")
     }
 
     /**
      * [kotlin의 Iterable 변수 선언 템플릿은 아래와 같습니다:]
      *
      * (val/var) (variable_name): List<(타입)> = (값)
-     * (값)
+     * (val/var) (variable_name): Set<(타입)> = (값)
      */
 
     @DisplayName("Iterator: List")
@@ -39,31 +39,74 @@ class ListTest {
         val list2 = mutableListOf("A", "B", "C")
 
         // 또는 java에서 사용했던 ArrayList 타입으로도 만들 수도 있습니다.
-        // 실제로 함수를 까보면 mutableListOf와 기능이 같고, return 타입이 다릅니다.
+        // 실제로 함수를 까보면 mutableListOf와 기능이 같지만 return 타입이 다릅니다.
         var list3 = arrayListOf("A", "B", "C")
 
         // listOf로 만든 변수는 List<T> 타입으로, Read-Only 속성입니다.
-        // 따라서 list의 요소를 변경할 수 있는 add, remove 함수가 없습니다.
+        // 따라서 요소를 변경할 수 있는 add, remove 함수가 없습니다.
 
         // mutableListOf로 만든 변수는 MutableList<T> 타입으로, List 안의 요소를 변형할 수 있습니다.
 
-        // [val]로 선언해도 요소를 변형할 수 있는데, [val]은 재선언만 불가능하기 때문입니다. (변수 선언 시엔 선언할 공간을 가리킬 포인터 주소가 새로 생성됩니다.)
+        // [val]로 선언해도 요소를 변경할 수 있는데, [val]은 재선언만 불가능하기 때문입니다. (변수 선언 시엔 선언할 공간을 가리킬 포인터 주소가 새로 생성됩니다.)
         // list2 = mutableListOf("22")
         list2.remove("B")
         list2.add("D")
 
+        // list 값을 그대로 출력에 사용할 수 있습니다.
+        println(list2)
+    }
+
+    @DisplayName("Iterator: 반복문으로 변수 꺼내기")
+    @Test
+    fun forEachPractice() {
+        val list: List<String?> = listOf("A", "B", null, "D")
+
         // 아래의 예시는 for {} 로 요소를 꺼내는 방법입니다.
-        for(it in list2) {
-            print(it)
+        for(it in list) {
+            print("$it ")
         }
         println()
 
-        // List는 Iterator형 클래스이므로 forEach 함수를 호출할 수 있습니다. 위의 for {} 문과 기능이 같습니다.
-        list3.forEach{
-            print(it)
+        // Iterator 클래스는 forEach 함수를 사용할 수 있습니다.
+        list.forEach {
+            println("[value: $it]")
         }
 
+        // 또한 [index]도 변수로 받는 forEachIndexed 함수도 지원합니다.
+        list.forEachIndexed { index, s ->
+            println("[index: $index][value: $s]")
+        }
         println()
+    }
+
+    @DisplayName("Iterator에서 지원하는 함수들")
+    @Test
+    fun iterableFunctions() {
+        val list1: List<String> = listOf("해", "뤼", "보")
+        val list2: List<String?> = listOf("해", "뤼", "보")
+
+        // sorted() / sortedDescending() 함수는 정렬된 list를 반환합니다.
+        // nullable 타입은 호출이 불가합니다.
+        println(list1.sorted())              // 오름차순
+        println(list1.sortedDescending())    // 내림차순
+        //println(list2.sorted())
+
+        // map() 함수는 요소의 값을 변형한 list를 반환합니다.
+        // nullable type의 List도 사용할 수 있습니다. String? 타입이면 null을 문자열로 판단합니다.
+        val newList1 = list1.map { it + 12345 }
+        val newList2 = list2.map { it + 12345 }
+        println("newList1: $newList1")
+        println("newList2: $newList2")
+
+        // 단, [Int]처럼 연산 작업 시엔 Safe Call을 하지 않거나 null을 확인하지 않으면 오류가 발생합니다.
+        val list3: List<Int?> = listOf(10, 20, null, 50, 40, 15)
+        //val newList3 = list3.map { it * 20 }
+        val newList3 = list3.map { it?.times(20) }
+        println(newList3)
+
+        // filter() 함수는 조건에 부합하는 값만 추출한 뒤 list로 반환합니다.
+        val filteredList = list2.filter { it?.startsWith("하") == true }
+        println(filteredList)
 
     }
 
@@ -80,7 +123,7 @@ class ListTest {
         }
         println()
 
-        // 만약 확장 함수 [let]을 빌려 [?] operator를 사용하면, null 단계에서 사용할 수 없기 되므로 print를 출력하지 않습니다.
+        // 만약 scope 함수인 [let]을 빌려 [?] operator를 사용하면, null 단계에서 사용할 수 없기 되므로 print를 출력하지 않습니다.
         list.forEach{
             it?.let { print("$it ") }
         }
--- src/test/kotlin/com/ennaru/practice/kotlin/VariableTest.kt
+++ src/test/kotlin/com/ennaru/practice/kotlin/VariableTest.kt
@@ -15,12 +15,12 @@ class VariableTest {
 
     @BeforeTest
     fun before() {
-        log.info("[Kotlin Test] ${javaClass.simpleName}")
+        log.info("[Kotlin Test] ${javaClass.simpleName} Start")
     }
 
     @AfterTest
     fun after() {
-        log.info("=====================================")
+        log.info("[Kotlin Test] ${javaClass.simpleName} End")
     }
 
     /**
@@ -74,12 +74,59 @@ class VariableTest {
         println("value2: $value2")
     }
 
+    // lateInit() 테스트를 위해 간이로 생성한 class
+    private class LateTest {
+        private lateinit var value: String
+        fun isInit(): Boolean {
+            return this::value.isInitialized
+        }
+        fun setValue(value: String) {
+            this.value = value
+        }
+    }
+
+    @DisplayName("변수: lateinit")
+    @Test
+    fun lateInit() {
+
+        // 만약 변수 선언과 동시에 값을 초기화하고 싶지 않을 때는 [lateinit]을 사용합니다.
+        lateinit var value1: String
+
+        // [lateinit]에는 제약사항이 존재합니다.
+
+        // [lateinit]으로 선언한 변수는 초기화와 상관없이 값을 꺼내는 로직을 넣으면 컴파일 오류가 발생하지 않습니다.
+        // 대신 런타임에서 값을 꺼내는 순간 UninitializedPropertyAccessException 에러가 반환됩니다.
+        try {
+            print(value1)
+        } catch(e: Exception) {
+            println("Exception! : ${e.javaClass.simpleName}")
+            println("Exception! : ${e.message}")
+        }
+
+        // [lateinit]은 mutable variable 만 허용합니다.
+        // 즉, 값을 변경할 수 없는 [val]로 정의할 수 없습니다.
+        //lateinit val value2: String
+
+        // [lateinit]은 Primitive Type 에선 사용할 수 없습니다.
+        // [Primitive Type]에는 [Byte] [Int] [Double] [Float] [Short] [Long] 이 있습니다.
+        //lateinit var value2: Int
+
+        // [lateinit]은 선언과 동시에 값을 초기화할 수 없습니다.
+        //lateinit var value2 = "#33"
+
+        // [lateinit]은 클래스 변수에 선언하고 사용하면 아래처럼 활용할 수 있습니다.
+        val lateTest: LateTest = LateTest()
+
+        // isInit() 함수에는 클래스에 선언한 변수에 [.isInitialized]를 호출하면 초기화 여부를 확인할 수 있습니다.
+        println("lateTest.isInit(): ${lateTest.isInit()}")
+        lateTest.setValue("50")
+        println("lateTest.isInit(): ${lateTest.isInit()}")
+
+    }
+
     @DisplayName("변수: ? Operator")
     @Test
     fun safeCallOperator1() {
-
-        // kotlin에서는 타입을 지정해 놓고 null을 넣으면 컴파일 에러가 발생합니다.
-        //var value1: String = null
 
         // 타입을 지정하면서 null을 허용해야 할 때 타입 뒤에 [?] operator를 붙입니다.
         // 아래의 변수는 [String?] 타입으로 정의됩니다.
@@ -91,6 +138,9 @@ class VariableTest {
 
         print("value2: $value2")
         print("value3: $value3")
+
+        // 만약 [?] operator를 붙이지 않는 타입을 지정해 놓고 null을 넣으면 컴파일 에러가 발생합니다.
+        //var value1: String = null
 
         // 만약 null로 값을 정의하면 [Nothing?] 타입으로 선언됩니다.
         // Nothing 타입은 '존재할 수 없는 값'을 의미하며 해당 변수로 Return Type으로 선언하면 '아무것도 반환하지 않겠다'를 의미합니다.
